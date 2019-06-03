65c808a0a252206f92222d44dbe8f935f0156f1f
Michael Jenkins <mjenkins9605@vidangel.com>
2019-06-02 22:29:57 -0400

    added GET

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ server.get("/", (req, res) => {
   );
 });
 
+//projects
+
+server.get("/api/projects", (req, res) => {
+    db("projects")
+      .then(projects => {
+        res.json(projects);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          message: "The project information could not be retrieved."
+        });
+      });
+  });
+
+  //actions
+
+server.get("/api/actions", (req, res) => {
+    db("actions")
+      .then(actions => {
+        res.json(actions);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          message: "The action information could not be retrieved."
+        });
+      });
+  });
+  
 const port = 5678;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
